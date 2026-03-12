a994fe4bab9ea79561a4b9c3e9e1ae97b23c4378
ines <ines.dahmani@esprit.tn>
2026-03-12 22:36:18 +0100

    WIP avant migration Angular 19 builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None  // ← Ajoute cette ligne
+
 })
 export class AppComponent {
   title = 'rh-management-system';
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.ts",
+      "./src/app/**/*.html",
+      "./src/app/**/*.ts",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
